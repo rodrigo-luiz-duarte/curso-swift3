230d9177c943e2622dba6218310a311848c11d9f
rodrigo <rodrigo.luizduarte@gmail.com>
2018-02-19 23:30:22 -0300

    Término da aula 1 do curso swift parte 1

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet
+    var nameField : UITextField!
+    
+    @IBOutlet
+    var happinessField : UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction
+    func add() {
+        let name = nameField.text
+        let happiness = happinessField.text
+        print("Eaten \(String(describing: name)) with \(String(describing: happiness))")
+    }
 }
 
